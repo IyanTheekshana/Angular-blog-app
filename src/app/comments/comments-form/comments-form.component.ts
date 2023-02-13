54a4eb0d7d3b1c4ee39c49fb7ae6ade80d32f840
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-13 18:07:14 +0100

    1° step -(readme file)

---
+++ src/app/comments/comments-form/comments-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-form',
+  templateUrl: './comments-form.component.html',
+  styleUrls: ['./comments-form.component.css']
+})
+export class CommentsFormComponent {
+
+}
